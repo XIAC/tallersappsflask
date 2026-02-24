be48e3954ced7cdb643b435bd8cefdca125d5b54
XIAC <x.i.a.c.1504@gmail.com>
2026-02-23 20:56:56 -0400

    creacion de una app web con flask

---
+++ routes.py
@@ -0,0 +1,19 @@
+from app import app
+from flask import render_template
+
+@app.route('/')
+@app.route('/index')
+def index():
+        return render_template('index.html', subtitulo = "Actidad en grupo TAI")
+
+@app.route('/sobrenosotros')
+def sobrenosotros():
+       return render_template('sobrenosotros.html')
+    
+@app.route('/saludo')
+def saludo():
+        return 'Hola bienvenido a Taller Apps '
+    
+@app.route('/usuario/<nombre>')
+def usuario(nombre):
+        return f'Hola{nombre} bienvenido a Taller Apps '
